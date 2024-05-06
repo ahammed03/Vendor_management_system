67dc09d9539f79f8de6e3b62577dd0939c8d70a7
ahammed03 <ahammedshaik0301@gmail.com>
2024-05-06 21:16:20 +0530

    fixed signals.py error

--- api/models.py
+++ api/models.py
@@ -1,7 +1,5 @@
 from django.db import models
-from django.dispatch import receiver
-from django.db.models.signals import post_save
-from .utils import update_fulfillment_rate,update_quality_rating_avg
+
 # Create your models here.
 class Vendor(models.Model):
     name = models.CharField(max_length=100)
@@ -38,18 +36,3 @@ class PurchaseOrder(models.Model):
 
 
 
-# Signal handler
-@receiver(post_save, sender=PurchaseOrder)
-def update_performance_metrics(sender, instance, created, **kwargs):
-    print("")
-    print("In SIgnal")
-    if not created:
-        vendor = instance.vendor
-        print(vendor)
-        print(instance.status)
-        if instance.status == 'completed':
-            update_fulfillment_rate(vendor,PurchaseOrder=sender)
-            print("Compledt")
-            if instance.delivery_date:
-                print(instance.delivery_date)
-                update_quality_rating_avg(vendor, instance.quality_rating,PurchaseOrder = sender)
