c58fdd973bea2109cc1440ca89365005c58f392e
ahammed03 <ahammedshaik0301@gmail.com>
2024-05-07 11:04:15 +0530

    created historic performance model

--- api/models.py
+++ api/models.py
@@ -36,3 +36,10 @@ class PurchaseOrder(models.Model):
 
 
 
+class HistoricPerformance(models.Model):
+    vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+    on_time_delivery_rate = models.FloatField(null = True)
+    quality_rating_avg = models.FloatField(null = True)
+    average_response_time =  models.FloatField(null = True)
+    fulfillment_rate =  models.FloatField(null = True)
