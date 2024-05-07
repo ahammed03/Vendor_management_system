f2951fdcf6b8858321c33e59b6a55c76c66724cc
ahammed03 <ahammedshaik0301@gmail.com>
2024-05-07 10:48:22 +0530

    avg response time logic

--- api/signals.py
+++ api/signals.py
@@ -1,19 +1,14 @@
 from django.dispatch import receiver
 from django.db.models.signals import post_save
-from .utils import update_fulfillment_rate,update_quality_rating_avg
+from .utils import update_fulfillment_rate,update_quality_rating_avg,update_average_response_time
 from .models import PurchaseOrder
 # Signal handler
 @receiver(post_save, sender=PurchaseOrder)
 def update_performance_metrics(sender, instance, created, **kwargs):
-    print("")
     print("In SIgnal")
     if not created:
         vendor = instance.vendor
-        print(vendor)
-        print(instance.status)
-        if instance.status == 'completed':
+        if instance.status == "completed":
             update_fulfillment_rate(vendor,PurchaseOrder=sender)
-            print("Compledt")
-            if instance.delivery_date:
-                print(instance.delivery_date)
-                update_quality_rating_avg(vendor, instance.quality_rating,PurchaseOrder = sender)
+            update_quality_rating_avg(vendor,PurchaseOrder = sender)
+        update_average_response_time(vendor,PurchaseOrder=sender)
--- api/utils.py
+++ api/utils.py
@@ -1,5 +1,6 @@
 
 from django.db import models
+from datetime import datetime
 
 
 # Helper functions to update performance metrics
@@ -12,7 +13,7 @@ def update_on_time_delivery_rate(vendor,PurchaseOrder):
         vendor.on_time_delivery_rate = (total_on_time / total_completed) * 100
         vendor.save()
 
-def update_quality_rating_avg(vendor, quality_rating, PurchaseOrder):
+def update_quality_rating_avg(vendor, PurchaseOrder):
     completed_orders = PurchaseOrder.objects.filter(vendor=vendor, status='completed')
     total_completed = completed_orders.count()
     total_quality_rating = completed_orders.aggregate(models.Avg('quality_rating'))['quality_rating__avg']
@@ -20,16 +21,20 @@ def update_quality_rating_avg(vendor, quality_rating, PurchaseOrder):
         vendor.quality_rating_avg = total_quality_rating
         vendor.save()
 
-def update_average_response_time(vendor, response_time,PurchaseOrder):
+def update_average_response_time(vendor,PurchaseOrder):
     completed_orders = PurchaseOrder.objects.filter(vendor=vendor, status = 'completed' or 'canceled', acknowledgment_date__isnull=False)
     total_completed = completed_orders.count()
-    total_response_time = completed_orders.aggregate(models.Avg(response_time))['{}__avg'.format(response_time)]
     if total_completed > 0:
-        vendor.average_response_time = total_response_time
+        time_differences = []
+        for order in completed_orders:
+            time_difference = dates_difference(order.acknowledgment_date,order.issue_date)
+            time_differences.append(time_difference) 
+        diff = sum(time_differences) / total_completed
+        vendor.average_response_time = diff
         vendor.save()
 
+
 def update_fulfillment_rate(vendor,PurchaseOrder):
-    print("Ful fill ment")
     completed_orders = PurchaseOrder.objects.filter(vendor=vendor, status='completed')
     successful_orders = completed_orders.exclude(quality_rating = 0)
     total_completed = completed_orders.count()
@@ -37,3 +42,14 @@ def update_fulfillment_rate(vendor,PurchaseOrder):
     if total_completed > 0 and total_successful > 0:
         vendor.fulfillment_rate = (total_successful / total_completed) * 100
         vendor.save()
+
+
+
+
+def dates_difference(end,start):
+    end_str = end.strftime('%Y-%m-%d %H:%M:%S')  # Remove microseconds
+    start_str = start.strftime('%Y-%m-%d %H:%M:%S')  # Remove microseconds
+    end_datetime = datetime.strptime(end_str, '%Y-%m-%d %H:%M:%S')
+    start_datetime = datetime.strptime(start_str, '%Y-%m-%d %H:%M:%S')
+    time_diff = end_datetime - start_datetime
+    return time_diff.total_seconds()
